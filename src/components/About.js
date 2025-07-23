dff7bb650feb64201ef2273af84e6277b51b4adb
nashville149 <milliamkarambu76@gmail.com>
2025-07-23 13:17:41 +0300

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
+// src/components/About.js
+
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love coding and creating web applications that solve real problems.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
